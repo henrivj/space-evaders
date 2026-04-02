37f5966422a9395d691ded24fa885018d28cfaf1
henrivj <henrique_justus@estudante.sesisenai.org.br>
2026-04-02 09:53:13 -0300

    minor changes 2

--- src/models/Game.js
+++ src/models/Game.js
@@ -1,113 +1,113 @@
-import Asteroid from './entities/Asteroid.js'
-import Star from './entities/Star.js'
-import { canvas } from '../index.js'
+import Asteroid from './entities/Asteroid.js';
+import Star from './entities/Star.js';
+import { canvas } from '../index.js';
 
 export default class Game {
 	constructor(players, levels, keysPressed) {
-		this.players = players
-		this.levels = levels
-		this.currentLevel = 0
-		this.score = 0
-		this.state = 'menu'
-		this.keysPressed = keysPressed
+		this.players = players;
+		this.levels = levels;
+		this.currentLevel = 0;
+		this.score = 0;
+		this.state = 'menu';
+		this.keysPressed = keysPressed;
 	}
 
 	reset() {
-		this.currentLevel = 0
-		this.score = 0
+		this.currentLevel = 0;
+		this.score = 0;
 
 		this.levels.forEach((level) => {
-			level.bgOffset = 0
+			level.bgOffset = 0;
 			level.clusters.forEach((cluster) => {
-				cluster.entities = cluster.generateEntities()
-			})
-		})
+				cluster.entities = cluster.generateEntities();
+			});
+		});
 
 		this.players.forEach((player, index) => {
-			player.reset(index)
-		})
+			player.reset(index);
+		});
 	}
 
 	update() {
-		this.handleGameState()
-		
-		if (this.state !== 'playing') return
+		this.handleGameState();
 
-		this.handlePlayerMovement()
+		if (this.state !== 'playing') return;
+
+		this.handlePlayerMovement();
 
 		this.players.forEach((player) => {
-			player.update()
-		})
+			player.update();
+		});
 
-		this.handlePlayerDeath()
-		this.handleLevelTransition()
-		this.handlePreviousLevels()
-		this.handleCurrentLevel()
+		this.handlePlayerDeath();
+		this.handleLevelTransition();
+		this.handlePreviousLevels();
+		this.handleCurrentLevel();
 	}
 
 	handleGameState() {
-		if (!this.keysPressed['Enter']) return
-		this.keysPressed['Enter'] = false
+		if (!this.keysPressed['Enter']) return;
+		this.keysPressed['Enter'] = false;
 
 		switch (this.state) {
 			case 'victory':
 			case 'defeat':
-				this.state = 'menu'
-				break
+				this.state = 'menu';
+				break;
 			case 'menu':
-				this.reset()
-				this.state = 'playing'
-				break
+				this.reset();
+				this.state = 'playing';
+				break;
 			case 'paused':
-				this.state = 'playing'
-				break
+				this.state = 'playing';
+				break;
 			case 'playing':
-				this.state = 'paused'
-				break
+				this.state = 'paused';
+				break;
 		}
 	}
 
 	handlePlayerMovement() {
-		const w = this.keysPressed['w']
-		const s = this.keysPressed['s']
-		const arrowUp = this.keysPressed['ArrowUp']
-		const arrowDown = this.keysPressed['ArrowDown']
+		const w = this.keysPressed['w'];
+		const s = this.keysPressed['s'];
+		const arrowUp = this.keysPressed['ArrowUp'];
+		const arrowDown = this.keysPressed['ArrowDown'];
 
-		if ((w && s) || (!w && !s)) this.players[0].direction = 0
-		else if (w) this.players[0].direction = -1
-		else if (s) this.players[0].direction = 1
+		if ((w && s) || (!w && !s)) this.players[0].direction = 0;
+		else if (w) this.players[0].direction = -1;
+		else if (s) this.players[0].direction = 1;
 
-		if ((arrowUp && arrowDown) || (!arrowUp && !arrowDown)) this.players[1].direction = 0
-		else if (arrowUp) this.players[1].direction = -1
-		else if (arrowDown) this.players[1].direction = 1
+		if ((arrowUp && arrowDown) || (!arrowUp && !arrowDown)) this.players[1].direction = 0;
+		else if (arrowUp) this.players[1].direction = -1;
+		else if (arrowDown) this.players[1].direction = 1;
 
 		if (this.players[0].collidesWith(this.players[1])) {
-			const p0 = this.players[0]
-			const p1 = this.players[1]
-			const isP0Above = p0.posY < p1.posY
+			const p0 = this.players[0];
+			const p1 = this.players[1];
+			const isP0Above = p0.posY < p1.posY;
 
 			// nao deixa p0 passar p1
-			if (isP0Above && p0.direction === 1) p0.direction = 0
-			if (!isP0Above && p0.direction === -1) p0.direction = 0
+			if (isP0Above && p0.direction === 1) p0.direction = 0;
+			if (!isP0Above && p0.direction === -1) p0.direction = 0;
 
 			// nao deixa p1 passar p0
-			if (isP0Above && p1.direction === -1) p1.direction = 0
-			if (!isP0Above && p1.direction === 1) p1.direction = 0
+			if (isP0Above && p1.direction === -1) p1.direction = 0;
+			if (!isP0Above && p1.direction === 1) p1.direction = 0;
 		}
 	}
 
 	handleLevelTransition() {
-		if (!this.levels[this.currentLevel].isComplete(this.score)) return
+		if (!this.levels[this.currentLevel].isComplete(this.score)) return;
 
-		this.levels[this.currentLevel].clusters.forEach((cluster) => cluster.drain())
+		this.levels[this.currentLevel].clusters.forEach((cluster) => cluster.drain());
 
 		if (this.levels[this.currentLevel + 1]) {
-			this.currentLevel++
-			this.levels[this.currentLevel].bgOffset = canvas.width
+			this.currentLevel++;
+			this.levels[this.currentLevel].bgOffset = canvas.width;
 		} else {
-			this.state = 'victory'
+			this.state = 'victory';
 			if (this.score > localStorage.getItem('highScore')) {
-				localStorage.setItem('highScore', this.score)
+				localStorage.setItem('highScore', this.score);
 			}
 		}
 	}
@@ -118,53 +118,53 @@ export default class Game {
 				this.players.forEach((player) => {
 					if (player.collidesWith(entity)) {
 						if (cluster.EntityType === Star) {
-							player.health = Math.min(player.health + Math.trunc(entity.size * cluster.spawnOffset), player.maxHealth)
-							this.score += 1000
+							player.health = Math.min(player.health + Math.trunc(entity.size * cluster.spawnOffset), player.maxHealth);
+							this.score += 1000;
 						} else {
-							player.health = Math.max(player.health - Math.trunc(entity.size), 0)
+							player.health = Math.max(player.health - Math.trunc(entity.size), 0);
 						}
 
 						if (isCurrentLevel) {
-							entity.reset()
+							entity.reset();
 						} else {
-							entity.kill()
+							entity.kill();
 						}
 					}
-				})
+				});
 
 				if (entity.alive && entity.hasPassedX(0)) {
 					if (cluster.EntityType === Asteroid && entity.alive) {
-						this.score += Math.trunc(entity.size)
+						this.score += Math.trunc(entity.size);
 					}
 					if (isCurrentLevel) {
-						entity.reset()
+						entity.reset();
 					} else {
-						entity.kill()
+						entity.kill();
 					}
 				}
-			})
-		})
+			});
+		});
 
-		level.clusters.forEach((cluster) => cluster.update())
+		level.clusters.forEach((cluster) => cluster.update());
 	}
 
 	handlePlayerDeath() {
 		this.players.forEach((player) => {
 			if (player.health <= 0) {
-				this.state = 'defeat'
+				this.state = 'defeat';
 
 				if (this.score > localStorage.getItem('highScore')) {
-					localStorage.setItem('highScore', this.score)
+					localStorage.setItem('highScore', this.score);
 				}
 			}
-		})
+		});
 	}
 
 	handleCurrentLevel() {
-		this.processLevel(this.levels[this.currentLevel], true)
+		this.processLevel(this.levels[this.currentLevel], true);
 	}
 
 	handlePreviousLevels() {
-		for (let i = 0; i < this.currentLevel; i++) this.processLevel(this.levels[i], false)
+		for (let i = 0; i < this.currentLevel; i++) this.processLevel(this.levels[i], false);
 	}
 }
